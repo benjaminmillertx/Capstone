f10744a47f4a7eadea0a80a752a208512fbc7156
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-08-05 11:47:00 -0500

    Create JobTitlesRepository.java

---
+++ MillerTalentAgency/RepositoryInterfaces/JobTitlesRepository.java
@@ -0,0 +1,6 @@
+package com.example.millertalentagency.repository;
+
+import com.example.millertalentagency.entity.JobTitles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobTitlesRepository extends JpaRepository<JobTitles, Long> {}
