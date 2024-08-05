6b9538e2dbdb7a1fc31155b5787e63a97eff31a8
Benjamin Miller <156035893+benjaminmillertx@users.noreply.github.com>
2024-08-05 11:48:51 -0500

    Create User service.java

---
+++ MillerTalentAgency/Service layer/User service.java
@@ -0,0 +1,25 @@
+package com.example.millertalentagency.service;
+
+import com.example.millertalentagency.entity.User;
+import com.example.millertalentagency.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}I'm 
